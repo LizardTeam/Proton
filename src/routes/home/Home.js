617b381618fb152e27cfd4f04c5037a8c81f5fb3
Avo <go.go.gg.rostislav@gmail.com>
2018-03-30 13:23:50 +0300

    changed the main page

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -12,6 +12,7 @@ import PropTypes from 'prop-types';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Home.css';
 
+
 class Home extends React.Component {
   static propTypes = {
     news: PropTypes.arrayOf(
@@ -26,23 +27,7 @@ class Home extends React.Component {
   render() {
     return (
       <div className={s.root}>
-        <iframe width="0%" height="0" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/91750827&color=%236c3210&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
-        <div className={s.container}>
-          <h1>React.js News</h1>
-          {this.props.news.map(item => (
-            <article key={item.link} className={s.newsItem}>
-              <h1 className={s.newsTitle}>
-                <a href={item.link}>{item.title}</a>
-              </h1>
-              <div
-                className={s.newsDesc}
-                // eslint-disable-next-line react/no-danger
-                dangerouslySetInnerHTML={{ __html: item.content }}
-              />
-
-            </article>
-          ))}
-        </div>
+        <iframe width="0%" height="0" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/82861323&color=%23250808&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
       </div>
     );
   }
