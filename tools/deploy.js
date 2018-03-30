100f56c846877ab59746acd48ec28a15a085607b
maab2198 <maab2198@gmail.com>
2018-03-30 13:09:03 +0300

    change gitignore

--- tools/deploy.js
+++ tools/deploy.js
@@ -16,9 +16,9 @@ import run from './run';
 // GitHub Pages
 const remote = {
   name: 'github',
-  url: 'https://github.com/<user>/<repo>.git',
+  url: 'https://github.com/LizardTeam/Proton.git',
   branch: 'gh-pages',
-  website: 'https://<user>.github.io/<repo>/',
+  website: 'https://LizardTeam.github.io/Proton/',
   static: true,
 };
 
